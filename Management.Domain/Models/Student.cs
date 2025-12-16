398577ee0bc0fdad2c76e708562ad313f5bb80b1
Asrorbek <asrorbekabdurashidov5@gmail.com>
2025-12-16 23:15:03 +0500

    Add project files.

---
+++ Management.Domain/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Management.Domain.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+
+        public string firstname { get; set; }
+
+        public string lastname { get; set; }
+     }
+}
